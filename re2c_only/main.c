6b51ee88ed77478c0432a986512d9d0a0e8c4ce5
apg360 <53176930+apg360@users.noreply.github.com>
2020-12-13 18:45:11 +0200

    changes

--- re2c_only/main.c
+++ re2c_only/main.c
@@ -6,20 +6,41 @@
 
 // https://re2c.org/manual/manual_c.html
 
+/*
+ https://stackoverflow.com/questions/2667648/what-does-it-mean-to-be-terminated-by-a-zero
+
+  What does it mean to be “terminated by a zero”?
+ 
+  Take the string Hi in ASCII. Its simplest representation in memory is two bytes:
+  0x48
+  0x69
+  But where does that piece of memory end? 
+
+  Unless you're also prepared to pass around the number of bytes in the string,
+  you don't know - pieces of memory don't intrinsically have a length.
+
+  So C has a standard that strings end with a zero byte, also known as a NUL character:
+  0x48
+  0x69
+  0x00
+
+  The string is now unambiguously two characters long, because there are two characters before the NUL.
+*/
 int main() {
     
-    printf("lex '_Zer0' result: %d\n", lex("_Zer0"));
-    printf("lex 'one two three' result: %d\n", lex("one two three"));
-    printf("lex '' result: %d\n", lex(""));
-    printf("lex 'one two three four' result: %d\n", lex("one two three four"));
+    printf("'_Zer0' result: %d\n", lex("_Zer0"));
+    printf("'one two three' result: %d\n", lex("one two three"));
+    printf(" '' result: %d\n", lex(""));
+    printf(" 'one two three four' result: %d\n", lex("one two three four"));
     
     char commandLine[100];
     while ( fgets(commandLine, 100, stdin) ) {
+        //commandLine without carriage return and 0 at end
+        char wordsProper[100];
         // remove carriage return and newline
-        //char newL[100];
-        //for (char *p=commandLine,j=0; *p; p++) if (*p != '\r' && *p != '\0' && *p != '\n') newL[j++]=*p;
+        for (char *p=commandLine,j=0; *p; p++) if (*p != '\r' && *p != '\x00' && *p != '\n') wordsProper[j++]=*p;
         
-        printf("Result > %d \n", lex(commandLine)  );
+        printf("Result> %d \n", lex(wordsProper)  );
         //
         //int tokenID = lex(commandLine);
         // exit when scanner return 0 when get EOF.
--- re2c_only/scanner.re
+++ re2c_only/scanner.re
@@ -1,22 +1,26 @@
 // re2c $INPUT -o $OUTPUT -i
+// https://fr.qaz.wiki/wiki/Re2c
+// https://www.systutorials.com/docs/linux/man/1-re2c
 
 int lex(const char *YYCURSOR)
 {
     int wordCount = 0; // count word, not letters
-    
 loop:
     /*!re2c                                      // start of re2c block
     re2c:define:YYCTYPE = char;                  // configuration
     re2c:yyfill:enable = 0;                      // configuration
     re2c:flags:case-ranges = 1;                  // configuration
-
-    *               { return -1; }               // default rule
-    [ \t\r\n]     {}                             // ignore any whitespace that shows up at the end //[^ \t\r\n]+
+    
+    end = "\x00";    // end of line
+    wsp = [ \t\r\n]; // whitespace
+    
+    *               { printf("err\n"); return -1; }  // default rule
+    wsp             { goto loop;}                    // ignore any whitespace that shows up at the end //[^ \t\r\n]+
+    end             { return wordCount; }            // whenever EOF
     // normal rule
-    [\x00]          { return wordCount; }        // whenever we find zero
+    "reset"         { printf("Reset ....");wordCount=0;return 0; } // reset counter
     [a-z]+          { ++wordCount; goto loop; }  //only letters are counted
     [ ]+            { goto loop; }               // everything else ignored
-    
     */
     
     /*ident = [a-zA-Z_][a-zA-Z_0-9]*; // named definition
