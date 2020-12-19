d582b69407bbc1e825df74957525e0641cda1da7
apg360 <53176930+apg360@users.noreply.github.com>
2020-12-19 06:49:02 +0200

    changes

--- thirdparty/tokuhirom_lemon+re2c/parser.h
+++
@@ -1,5 +0,0 @@
-#define TOKEN_ADD                             1
-#define TOKEN_SUB                             2
-#define TOKEN_MUL                             3
-#define TOKEN_DIV                             4
-#define TOKEN_INT                             5
---
+++ tokuhirom_mycalc_lemon+re2c_ported to C/README.md
@@ -0,0 +1,1 @@
+C version of https://github.com/tokuhirom/re2c-lemon-tutorial
