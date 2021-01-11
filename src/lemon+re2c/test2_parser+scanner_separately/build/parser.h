da1a62fdb6a5004863ce2ed73e180995f3c9e0b6
apg360 <53176930+apg360@users.noreply.github.com>
2021-01-11 23:58:30 +0200

    changes

--- src/lemon+re2c/test2_parser+scanner_separately/build/parser.h
+++
@@ -1,8 +0,0 @@
-#define ADD                             1
-#define SUB                             2
-#define MUL                             3
-#define DIV                             4
-#define NEWLINE                         5
-#define LPAREN                          6
-#define RPAREN                          7
-#define INT_LITERAL                     8
