f1c847457b2b544e42df8c1110eb432ae864c6e5
apg360 <53176930+apg360@users.noreply.github.com>
2021-01-15 10:48:24 +0200

    changes

--- src/lemon+re2c/test3_parser+scanner_integrated/src/main.c
+++ src/lemon+re2c/test3_parser+scanner_integrated/src/main.c
@@ -78,28 +78,28 @@ void validation_test()
 
 int main() {
 
-    scanner_DebugMode=true;
+    scanner_DebugMode=false;
     PARSER_INIT(false);
     
-    //validation_test();
+    validation_test();
     
-    //SCANNER("50+125");
-    //SCANNER("15/5");
-    //SCANNER("50*125-50+125");
-    //SCANNER(" 4 + 5 - 6 + 7\n \
+    SCANNER("50+125");
+    SCANNER("15/5");
+    SCANNER("50*125-50+125");
+    SCANNER(" 4 + 5 - 6 + 7\n \
 (24/2)*5\n \
 ");
     
     //Process test.dat
     SCANNER( readFile() );
     
-    /*char *commandLine;
+    char *commandLine;
     while (  (commandLine = dynamic_fgets())  ){
       
       SCANNER(commandLine);
       
       free(commandLine); //dynamic_fgets do malloc, free the allocated memory now that variable is no more used
-    }*/
+    }
     
     PARSER_FREE();
     return 0;
