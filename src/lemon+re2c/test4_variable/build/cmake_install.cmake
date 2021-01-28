3d018c09c34c0df7571f3bca1df9300b43aa352f
apg360 <53176930+apg360@users.noreply.github.com>
2021-01-28 12:59:57 +0200

    changes

--- src/lemon+re2c/test4_variable/build/cmake_install.cmake
+++ src/lemon+re2c/test4_variable/build/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/manj20/Documents/GitHub/re2c-lemon-example/src/lemon+re2c/test5_variable
+# Install script for directory: /home/manj20/Documents/GitHub/re2c-lemon-example/src/lemon+re2c/test4_variable
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -50,5 +50,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/manj20/Documents/GitHub/re2c-lemon-example/src/lemon+re2c/test5_variable/build/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/manj20/Documents/GitHub/re2c-lemon-example/src/lemon+re2c/test4_variable/build/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
