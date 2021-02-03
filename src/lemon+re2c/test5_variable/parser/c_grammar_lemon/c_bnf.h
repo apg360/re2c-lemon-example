dbc79d2e5323726684c5ab2c3d8ac3f5c2fcb3b0
apg360 <53176930+apg360@users.noreply.github.com>
2021-02-03 12:42:09 +0200

    changes

---
+++ src/lemon+re2c/test5_variable/parser/c_grammar_lemon/c_bnf.h
@@ -0,0 +1,6 @@
+#define TK_IDENTIFIER                       1
+#define TK_CONSTANT                         2
+#define TK_STRINGLITERAL                    3
+#define TK_LPAREN                           4
+#define TK_RPAREN                           5
+#define TK_VALUE                            6
