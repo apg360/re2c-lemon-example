9a4a614003c1975747c9f8f3ad158670fbab4d89
apg360 <53176930+apg360@users.noreply.github.com>
2021-02-14 13:55:03 +0200

    changes

--- src/lemon+re2c/test5_C_grammar/include/token.h
+++ src/lemon+re2c/test5_C_grammar/include/token.h
@@ -6,18 +6,25 @@
 
 struct _scanner_state
 {
-    const char *cursor;                 // current_char : the next input character to be read.  A pointer-like l-value that stores the current input position
-    const char *begin;                  // first_char
+    /*
+    Example : 50+125
+    STEP1: lexemes : 50+125, cursor : +125
+    STEP2: lexemes : +125, cursor : 125
+    STEP3: lexemes : 125, cursor :
+    STEP4: lexemes : , cursor :
+    */
+    const char *linetext;               // The full string of current line being scanned
+    const char *lexemes;                // unconsumed part -> the next input remaining to be scanned.
+    const char *cursor;                 // the next input character to be read.  A pointer-like l-value that stores the current input position
+    const char *marker;                 // marker - the position of the most recent match
     const char *end;                    // last_char, limit, YYLIMIT  : the position after the last available input character.   A pointer-like r-value that stores the end of input position
                                         //                Lexer compares YYCURSOR to YYLIMIT in order to determine if there is enough input characters left
-    const char *marker;                 // marker - the position of the most recent match
-    const char *lexemes;                // unconsumed part -> the next input remaining to be scanned.
     
     int line_number, column_position;   // line number, column position
     
-    int token_id;                       // int value as found in <parser.h>
-    const char * token_name;            // string name as found in <parser.h>
-    char token_value[255];              // the current assigned value. No more than 255 characters
+    int token_id, token_text_len;       // int value of the token as found in <parser.h>
+    const char *token_name;             // string name of the token as found in <parser.h>
+    char token_stringvalue[255];        // the text value of the token. No more than 255 characters
 };
 
 struct _parser_state {
